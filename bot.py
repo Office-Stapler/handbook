ac659c9c4dfeebb6932d389ef3ffd94dfa6e895c
Raymond Lam <z5255290@unsw.edu.au>
2020-05-04 19:41:28 +1000

    Inital Commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord 
+from discord.ext import commands
+from commands import search
+token = ''
+
+
+
+
+
+Bot = commands.Bot('--')
+
+@Bot.event
+async def on_ready():
+    print('-----------------------')
+    print(f'     Logged in')
+    print('-----------------------')
+
+@Bot.command(name='search')
+async def search_code(ctx, subject):
+    info = search.search(subject)
+    if info == None:
+        await ctx.send('Please enter a valid course code!')
+    elif isinstance(info, list):
+        e = discord.Embed(
+            title=f'Couldn\'t find {subject}, but we found courses within the faculty',
+            description=str([x[0] for x in info]).replace('\'', ''),
+            colour=0xFFFF00
+        )
+        await ctx.send(embed=e)
+    else:
+        e = discord.Embed(
+            title=f'About {subject.upper()}: {info["name"]}',
+            description=info['overview'],
+            colour=discord.Color(0x000ff)
+        )
+        e.add_field(name='Offering Terms', value=info['terms'])
+        await ctx.send(embed=e)
+Bot.run(token)
---
+++ commands/search.py
@@ -0,0 +1,46 @@
+import bs4
+import json
+import requests
+
+def search(query):
+    ''' 
+        Arguments: query - A string that will be used to search the handbook \n
+        Return: 
+        Dictionary {
+            'overview': Course overview,
+            'terms': Course offering terms,
+            'name': Course name
+        } \n
+        If the subject faculty code is not found it returns None, \n
+        if the subject faculty code is found but the course code isn't it returns the list of courses
+    '''
+    query = query.upper()
+    url = 'https://www.handbook.unsw.edu.au/undergraduate/courses/2020/'
+    fac_code = query[0:4]
+    with open('subjectinfo.json', 'r') as f:
+        subjects = json.load(f)
+    if fac_code not in subjects:
+        return None
+    
+    if query not in [x[0] for x in subjects[fac_code]]:
+        return subjects[fac_code]
+    for subject in subjects[fac_code]:
+        if subject[0] == query:
+            name = subject[1]
+    page = requests.get(url + query).text
+    soup = bs4.BeautifulSoup(page, 'html.parser')
+    info = soup.find_all('div', class_='readmore__wrapper')
+    overview = info[0].get_text().strip()
+    offering = soup.find_all('p', class_='')
+
+    for offer in offering:
+        if 'Term' in offer.get_text():
+            offering = offer.get_text()
+            break
+    return {
+        'overview': overview,
+        'terms': offering,
+        'name': name
+    }
+
+
