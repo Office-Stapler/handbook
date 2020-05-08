ac98c3b0fd6a25c4a1160f37e100a33ddecf3b29
Raymond Lam <z5255290@unsw.edu.au>
2020-05-08 21:46:27 +1000

    Added a 'condition for enrolments' section

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import json
 from discord.ext import commands
 from commands import search
-
+from commands import countdown
 with open('config.json', 'r') as f:
     config = json.load(f)
 TOKEN = config['token']
@@ -33,7 +33,13 @@ async def search_code(ctx, subject):
             colour=discord.Color(0x000ff)
         )
         e.add_field(name='Offering Terms', value=info['terms'])
+        e.add_field(name='Conditions for Enrolment', value='None found' if info['prereq'] == [] else info['prereq'][0])
         await ctx.send(embed=e)
 
+@BOT.command(name='results')
+async def get_time(ctx):
+    diff = countdown.countdown()
+    if diff.days > 0:
+        await ctx.send(f'There are {diff.days} days left')
 if __name__ == "__main__":
     BOT.run(TOKEN)
