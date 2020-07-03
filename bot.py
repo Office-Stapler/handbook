9d48aa542377e21e4d7f38784aa7cd74a20e224a
Office-Stapler <raymond.lam@outlook.com.au>
2020-07-03 23:59:49 +1000

    Added new functionality for searching with name

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord 
 import json
 from discord.ext import commands
-from commands import search
+from commands import code
 from commands import countdown
+from commands import search
 with open('config.json', 'r') as f:
     config = json.load(f)
 TOKEN = config['token']
@@ -14,9 +15,9 @@ async def on_ready():
     print(f'     Logged in')
     print('-----------------------')
 
-@BOT.command(name='search')
+@BOT.command(name='code')
 async def search_code(ctx, subject):
-    info = search.search(subject)
+    info = code.search(subject)
     if not info:
         await ctx.send('Please enter a valid course code!')
     elif isinstance(info, list):
@@ -29,13 +30,30 @@ async def search_code(ctx, subject):
     else:
         e = discord.Embed(
             title=f'About {subject.upper()}: {info["name"]}',
-            description=info['overview'],
+            description=info['overview'][:2048],
             colour=discord.Color(0x000ff),
             url=info['url']
         )
         e.add_field(name='Offering Terms', value=info['terms'], inline=False)
         e.add_field(name='Conditions for Enrolment', value='None' if info['prereq'] == [] else info['prereq'], inline=False)
         await ctx.send(embed=e)
+
+@BOT.command(name='search')
+async def serach_name(ctx, subject):
+    info = search.name_search(subject)
+    if not info:
+        await ctx.send(f'No courses were found with the name {subject}')
+    else:
+        e = discord.Embed(
+            title=f'List of subjects with {subject.capitalize()}',
+            colour=discord.Color(0x000ff),
+        )
+        for subject in info:
+            e.add_field(name=subject['name'], value=subject['code'])
+        try:
+            await ctx.send(embed=e)
+        except discord.HTTPException:
+            await ctx.send('Your search was too vague, please be more specific')
 
 @BOT.command(name='results')
 async def get_time(ctx):
--- commands/search.py
+++ commands/search.py
@@ -1,110 +1,14 @@
-import bs4
 import json
-import requests
+subjects = None
 
-SUBJECT_INFO = {}
-
-UGRAD_URL = 'https://www.handbook.unsw.edu.au/undergraduate/courses/2020/'
-PGRAD_URL = 'https://www.handbook.unsw.edu.au/postgraduate/courses/2020/'
-
-def parse_subject_info():
-    """Parses the course information from JSON into a Python dict.
-
-    Returns:
-        array[array[dict]]: [{
-            faculty_name: [
-                {
-                    "code": 8 character course code,
-                    "name": Full course name,
-                    "uoc": Unit of credits
-                }
-            ]
-        }]
-    """
-
-    subject_info = {"faculties": set()}
-
-    with open('subjectinfo.json', 'r') as f:
-        subjects = json.load(f)
-
+def name_search(name):
+    global subjects
+    if not subjects:
+        with open('subjectinfo.json', 'r') as f:
+            subjects = json.load(f)
+    found = []
     for faculty in subjects:
-        subject_info[faculty] = subjects[faculty]
-        subject_info["faculties"].add(faculty)
-
-    return subject_info
-
-
-def get_handbook_details(query):
-    """Gets a more detailed description of a course from the handbook. 
-
-    Args:
-        query (str) - course code (possibly partial) to search the handbook.
-
-    Returns:
-        tuple: (overview, offering)
-    """
-    url = UGRAD_URL + query
-    page = requests.get(UGRAD_URL + query)
-    if page.status_code != 200:
-        page = requests.get(PGRAD_URL + query)
-        url = PGRAD_URL + query
-    soup = bs4.BeautifulSoup(page.text, 'html.parser')
-    #print(soup.get_text())
-    info = soup.find_all('div', class_='readmore__wrapper')
-    #print(info)  
-    overview = info[0].get_text().strip()
-    
-    offering = soup.find_all('p', class_='')
-
-    for offer in offering:
-        if 'Term' in offer.get_text():
-            offering = offer.get_text()
-            break
-    
-    return overview, offering, url
-
-
-def search(query):
-    ''' 
-    Args:
-        query (str) - course code (possibly partial) to search the handbook.
-    
-    Returns: 
-        dict: {
-            'overview': Course overview,
-            'terms': Course offering terms,
-            'name': Course name
-        }
-
-        None if faculty code is not found.
-        list if faculty code found, but course code not found.
-    '''
-
-    query = query.upper()
-    faculty_code = query[0:4]
-
-    global SUBJECT_INFO
-    if not SUBJECT_INFO:
-        SUBJECT_INFO = parse_subject_info()
-    subjects = SUBJECT_INFO
-
-    if faculty_code not in SUBJECT_INFO["faculties"]:
-        return None
-    
-    if query not in [subject["code"] for subject in subjects[faculty_code]]:
-        return [subject["code"] for subject in subjects[faculty_code]]
-        
-    for subject in subjects[faculty_code]:
-        if subject["code"] == query:
-            name = subject["name"]
-            prereq = subject['prereq']
-
-    overview, offering, url = get_handbook_details(query)
-
-    return {
-        'overview': overview,
-        'terms': offering,
-        'name': name,
-        'prereq': prereq,
-        'url': url
-    }
+        for subject in subjects[faculty]:
+            if name.lower() in subject['name'].lower():
+                found.append(subject)
+    return found
