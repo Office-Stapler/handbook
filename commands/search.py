a96d98301f7b39b67d4e8473ff0bee05a586366e
Office-Stapler <raymond.lam@outlook.com.au>
2020-07-17 13:08:54 +1000

    Update

--- commands/search.py
+++ commands/search.py
@@ -4,7 +4,7 @@ subjects = None
 def name_search(name):
     global subjects
     if not subjects:
-        with open('subjectinfo.json', 'r') as f:
+        with open('../subjectinfo.json', 'r') as f:
             subjects = json.load(f)
     found = []
     for faculty in subjects:
@@ -12,3 +12,8 @@ def name_search(name):
             if name.lower() in subject['name'].lower():
                 found.append(subject)
     return found
+
+if __name__ == '__main__':
+    found = name_search(input())
+    for subject in found:
+        print(subject['code'], subject['name'])
